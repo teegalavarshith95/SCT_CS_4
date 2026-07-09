8d6afdfd6a1ff54a447bf9a0f6ba89126a680a11
Teegala Varshith <teegalavarshith95@gmail.com>
2026-07-09 22:09:41 +0530

    Create Terminalcommand.py

---
+++ Terminalcommand.py
@@ -0,0 +1,11 @@
+PS C:\Users\Teegala Varshith\OneDrive\Desktop\Command_Logger_Project> python command_logger.py
+===== COMMAND LOGGER =====
+Type exit to stop.
+
+Enter command: Hello
+Saved successfully!
+Enter command: Python
+Saved successfully!
+Enter command: exit
+Logging stopped.
+PS C:\Users\Teegala Varshith\OneDrive\Desktop\Command_Logger_Project> 
